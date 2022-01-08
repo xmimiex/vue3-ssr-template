925d6eeef767204161b221bda52bfbce7ef6d873
Jean-Charles FAUCHET <jcfauchet@maisonsdumonde.com>
2022-01-08 22:34:45 +0100

    feat(server): get default from config

--- src/server/middlewares/country-language/utils.ts
+++ src/server/middlewares/country-language/utils.ts
@@ -1,29 +1,24 @@
 import { KoaContext } from '../../typings/server'
+import { appConf } from '../../utils/config'
 
 const requestIp = require('request-ip')
 const geoip = require('geoip-country')
 
-export const DEFAULT_COUNTRY = 'FR'
-export const DEFAULT_LANGUAGE = 'fr'
+const {
+  internationalization: {
+    languagesForCountry,
+  },
+} = appConf
 
-interface CountryLanguages {
-  [key: string]: string[];
-}
-const COUNTRY_LANGUAGES_TABLE: CountryLanguages = {
-  FR: [ 'fr' ],
-  UK: [ 'en' ],
-  CH: [ 'fr', 'it', 'en' ],
-}
+export const isCountrySupported = (country: string) => Boolean(languagesForCountry[country]?.length ?? false)
+export const isCountryLanguageSupported = (country: string, language: string) => languagesForCountry[country]
+  ?.includes(language) ?? false
 
-export const isCountrySupported = (country: string) => Boolean(COUNTRY_LANGUAGES_TABLE[country]?.length ?? false)
-export const isCountryLanguageSupported = (country: string, language: string) =>
-  COUNTRY_LANGUAGES_TABLE[country]?.includes(language) ?? false
-
-const getDefaultCountryLanguage = (country: string) => COUNTRY_LANGUAGES_TABLE[country]?.[0]
+const getDefaultCountryLanguage = (country: string) => languagesForCountry[country]?.[0]
 export const detectLanguage = (country: string, acceptLanguages: string | undefined) => {
   if (!acceptLanguages) return getDefaultCountryLanguage(country)
 
-  const eligibleLanguages = (COUNTRY_LANGUAGES_TABLE[country] || [])
+  const eligibleLanguages = (languagesForCountry[country] || [])
     .filter(language => acceptLanguages.includes(language))
 
   return eligibleLanguages ? eligibleLanguages[0] : getDefaultCountryLanguage(country)
